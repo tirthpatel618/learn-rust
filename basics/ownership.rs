9badf2f39803b630b60cb39b224663e08b678f68
Tirth Patel <t83patel@uwaterloo.ca>
2026-04-28 16:40:42 -0400

    learning ownership

---
+++ basics/ownership.rs
@@ -0,0 +1,15 @@
+
+
+fn main() {
+    let name = String::from("Tirth");
+    let other = name; // MOVE
+
+    println("{}", other) // this will work because other is now the owner of the string
+    // string owns heap memory, rust moves ownership
+    // println("{}", name) // this will not work because name is no longer the owner of the string
+    print_name(&other); // we can pass a reference to other, which allows us to use the string without taking ownership
+}
+
+fn print_name(name: &String) {
+    println!("Name: {}", name);
+}
--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let todo = Todo {
+        id: 1,
+        title: String::from("Learn Rust"),
+        completed: false,
+    };
+    print_todo_title(&todo.title);
+    println!("Item: {}", todo.title); // this will work because we are still the owner of the string)
 }
 
 
@@ -7,4 +14,8 @@ struct Todo {
     id: u64,
     title: String,
     completed: bool,
+}
+
+fn print_todo_title(title: &str) {
+    println!("Item: {}", title);
 }
