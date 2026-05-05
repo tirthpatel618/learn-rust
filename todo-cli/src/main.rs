9fddf23e592aea4ccb8c5f2a61dcbeb074667bfc
Tirth Patel <t83patel@uwaterloo.ca>
2026-05-05 14:26:06 -0400

    chore: quick commit 2026-05-05 14:26:06

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,21 +1,71 @@
-fn main() {
-    println!("Hello, world!");
-    let todo = Todo {
-        id: 1,
-        title: String::from("Learn Rust"),
-        completed: false,
-    };
-    print_todo_title(&todo.title);
-    println!("Item: {}", todo.title); // this will work because we are still the owner of the string)
-}
+use std::env;
+use std::error::Error;
 
-
+#[derive(Debug)]
 struct Todo {
     id: u64,
     title: String,
     completed: bool,
 }
 
-fn print_todo_title(title: &str) {
-    println!("Item: {}", title);
+#[derive(Debug)]
+enum Command {
+    Add(String),
+    List,
+    Done(u64),
+    Remove(u64),
+}
+
+fn parse_command(args: &[String]) -> Result<Command, String> {
+    if args.len() < 2 {
+        return Err(String::from("expected a command"));
+    }
+
+    match args[1].as_str() {
+        "add" => {
+            if args.len() < 3 {
+                return Err(String::from("expected a todo title"));
+            }
+
+            Ok(Command::Add(args[2].clone()))
+        }
+        "list" => Ok(Command::List),
+        "done" => {
+            if args.len() < 3 {
+                return Err(String::from("expected todo id"));
+            }
+
+            let id = parse_id(&args[2])?;
+            Ok(Command::Done(id))
+        }
+        "remove" => {
+            if args.len() < 3 {
+                return Err(String::from("expected todo id"));
+            }
+
+            let id = parse_id(&args[2])?;
+            Ok(Command::Remove(id))
+        }
+        other => Err(format!("unknown command: {}", other)),
+    }
+}
+
+fn parse_id(input: &str) -> Result<u64, String> {
+    input
+        .parse::<u64>()
+        .map_err(|_| String::from("todo id must be a number"))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let command = parse_command(&args)?;
+
+    match command {
+        Command::Add(title) => println!("Add: {}", title),
+        Command::List => println!("List todos"),
+        Command::Done(id) => println!("Done: {}", id),
+        Command::Remove(id) => println!("Remove: {}", id),
+    }
+
+    Ok(())
 }
